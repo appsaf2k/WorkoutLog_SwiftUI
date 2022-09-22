004e2023cfb8cc94ef28f0759b9e5d52808a2038
appsaf2k <appsaf2k@gmail.com>
2022-09-22 10:30:32 +0300

    Initial Commit

---
+++ WorkoutLog/WorkoutLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutLogApp.swift
+//  WorkoutLog
+//
+//  Created by @andreev2k on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
